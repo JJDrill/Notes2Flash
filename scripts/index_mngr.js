6ffd4a4e72cddbb0257e6fc56aa063ded77336e6
J. Drill <JJDrill@hotmail.com>
2015-11-17 13:30:00 -0700

    New nodes can be added now, but there is a bug

---
+++ scripts/index_mngr.js
@@ -0,0 +1,32 @@
+var Index_Manager = function() {
+  var KEY_NOTEBOOK_INDEX = "Index_Notebook"
+  var KEY_NOTE_INDEX = "Index_Note"
+
+  var currentNotebookIndex = localStorage.getItem(KEY_NOTEBOOK_INDEX)
+  currentNotebookIndex = parseInt(currentNotebookIndex, 10) || 0
+
+  var currentNoteIndex = localStorage.getItem(KEY_NOTE_INDEX)
+  currentNoteIndex = parseInt(currentNoteIndex, 10) || 0
+
+console.log(currentNotebookIndex);
+console.log(currentNoteIndex);
+  /*
+  Gets the current max notebook ID from local storage and increments it
+  */
+  this.Get_New_Notebook_ID = function() {
+    currentNotebookIndex += 1
+    localStorage.setItem(KEY_NOTEBOOK_INDEX, currentNotebookIndex)
+console.log(currentNotebookIndex);
+    return "NB" + currentNotebookIndex
+  }
+
+  /*
+  Gets the current max note ID from local storage and increments it
+  */
+  this.Get_New_Note_ID = function() {
+    currentNoteIndex += 1
+    localStorage.setItem(KEY_NOTE_INDEX, currentNoteIndex)
+console.log(currentNoteIndex);
+    return "NT" + currentNoteIndex
+  }
+}
--- scripts/notes.js
+++ scripts/notes.js
@@ -1,4 +1,9 @@
 $(function(){
+  var NEW_NOTEBOOK_ID = "NEW_NOTEBOOK";
+  var NEW_NOTEBOOK_TEXT = "* Add New Notebook *";
+  var NEW_NOTE_ID = "NEW_NOTE_";
+  var NEW_NOTE_TEXT = "* Add New Note *";
+
   /*
   Get all the content from the note text area
   */
@@ -76,17 +81,34 @@ $(function(){
     // build the data array
     var dataArray = []
 
-    //for (var i = listData.length; i >= 0; i--) {
+    // add the default "add new note" node for each notebook at the start
+    var defaultNewNotebook = {
+      "id" : NEW_NOTEBOOK_ID,
+      "parent" : "#",
+      "text" : NEW_NOTEBOOK_TEXT
+    }
+    dataArray.push(defaultNewNotebook)
+
     for (var i = 0; i < listData.length; i++) {
       // for notebook objects
       if (listData[i] instanceof notebookMngr.objNotebook) {
+
+        // add the default "add new note" node for each notebook at the start
+        var defaultNewNote = {
+          "id" : NEW_NOTE_ID + listData[i].notebook_id,
+          "parent" : listData[i].notebook_id,
+          "text" : NEW_NOTE_TEXT
+        }
+        dataArray.push(defaultNewNote)
+
+        //now add the notebook node
         var newItem = {
           "id" : listData[i].notebook_id,
           "parent" : "#",
           "text" : listData[i].notebook_name
         }
-        console.log(newItem);
         dataArray.push(newItem)
+
 
       // for note objects
       } else if (listData[i] instanceof notebookMngr.objNote) {
@@ -98,10 +120,17 @@ $(function(){
         dataArray.push(newItem)
       }
     }
+var temp = [];
+console.log(dataArray);
 
+    $('#jstree_demo_div').jstree('deselect_all');
     $('#jstree_demo_div').jstree({ 'core' : {
         'data' : dataArray
     } });
+    $('#jstree_demo_div').jstree(true).redraw(true);
+    //$('#jstree_demo_div').jstree(true).refresh();
+
+    //$('#jstree_demo_div').jstree('refresh');
     // $('#jstree_demo_div').jstree({ 'core' : {
     //     'data' : [
     //        {  "id" : "ajson1", "parent" : "#", "text" : "History" },
@@ -136,30 +165,60 @@ $(function(){
   })
 
   $('#jstree_demo_div').on("changed.jstree", function (e, data) {
-//console.log(data);
-//console.log(e);
+
     if (data.action === 'select_node') {
+
       //check if it's a note, if it is open it!
       if (data.selected[0].substring(0,2) === "NT") {
         //save the content first if the node changed
         if (currentSelectedNodeID !== data.selected[0] &&
             currentSelectedNodeID !== "") {
-console.log('here!!!!!!!!!!!!!!' + currentSelectedNodeID);
           var noteToUpdate = notebookMngr.DB_Get_Note(currentSelectedNodeID)
-console.log(Get_Notes('string'));
           noteToUpdate.notes = Get_Notes('string');
           notebookMngr.DB_Save_Note(noteToUpdate)
         }
 
         var noteToOpen = notebookMngr.DB_Get_Note(data.selected[0])
-  //console.log(data.selected[0]);
         Put_Notes(noteToOpen.notes)
         currentSelectedNodeID = data.selected[0];
+
+      // check if the user clicked on the new notebook node
+      } else if (data.selected[0].substring(0,NEW_NOTEBOOK_ID.length) === NEW_NOTEBOOK_ID) {
+        var newNotebookName = prompt("Please enter the new notebook name", "New Notebook");
+        if (newNotebookName != null) {
+          var newBook = new notebookMngr.objNotebook();
+          newBook.notebook_id = indexMngr.Get_New_Notebook_ID();;
+          newBook.notebook_name = newNotebookName;
+          notebookMngr.DB_Save_Notebook(newBook)
+
+          //refresh the tree
+          var fullList = notebookMngr.DB_Get_Full_List();
+          Update_Tree(fullList)
+        }
+
+      // check if the user clicked on the new note node
+      } else if (data.selected[0].substring(0,NEW_NOTE_ID.length) === NEW_NOTE_ID) {
+// console.log(data);
+// console.log(e);
+        var newNotebookName = prompt("Please enter the new note name", "New Note");
+        if (newNotebookName != null) {
+          var newNote = new notebookMngr.objNote();
+          newNote.noteId = indexMngr.Get_New_Note_ID();
+          newNote.refNotebookId = data.node.parents[0];
+          newNote.noteName = newNotebookName;
+          newNote.notes = "";
+          notebookMngr.DB_Save_Note(newNote)
+
+          //refresh the tree
+          var fullList = notebookMngr.DB_Get_Full_List();
+          Update_Tree(fullList)
+        }
       }
     }
   });
 
 
+  var indexMngr = new Index_Manager();
   var notebookMngr = new Notebook_Mngr();
   var currentSelectedNodeID = "";
 
@@ -167,25 +226,31 @@ console.log(Get_Notes('string'));
   Insert some test data if they have an empty local storage
   */
   if (localStorage.length === 0) {
-    console.log("temp data instereted");
+
+    var Book1ID = indexMngr.Get_New_Notebook_ID();
     var myBook1 = new notebookMngr.objNotebook();
-    myBook1.notebook_id = "NB0";
-    myBook1.notebook_name = "Book 1";
+    myBook1.notebook_id = Book1ID;
+    myBook1.notebook_name = "Default Notebook #1";
     notebookMngr.DB_Save_Notebook(myBook1)
 
     var myNote = new notebookMngr.objNote();
-    myNote.noteId = "NT0";
-    myNote.refNotebookId = "NB0";
+    myNote.noteId = indexMngr.Get_New_Note_ID();
+    myNote.refNotebookId = Book1ID;
     myNote.noteName = "Note 1";
-    myNote.notes = "Test note data";
+    myNote.notes = "Test note data 1";
     notebookMngr.DB_Save_Note(myNote)
 
     var myNote1 = new notebookMngr.objNote();
-    myNote1.noteId = "NT1";
-    myNote1.refNotebookId = "NB0";
+    myNote1.noteId = indexMngr.Get_New_Note_ID();
+    myNote1.refNotebookId = Book1ID;
     myNote1.noteName = "Note 2";
     myNote1.notes = "Test note data 2";
     notebookMngr.DB_Save_Note(myNote1)
+
+    var myBook2 = new notebookMngr.objNotebook();
+    myBook2.notebook_id = indexMngr.Get_New_Notebook_ID();
+    myBook2.notebook_name = "Default Notebook #2";
+    notebookMngr.DB_Save_Notebook(myBook2)
   }
 
   var fullList = notebookMngr.DB_Get_Full_List();
